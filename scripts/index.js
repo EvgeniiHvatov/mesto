8866e12924eea9cd906b70e1b45f3aa8a1978dd6
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-03-27 21:53:39 +0300

    Добавить карточки, функцию удаления, попап,

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,3 @@
-let popup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
-
-let openPopupButton = profile.querySelector('.profile__edit-button');
-let closePopupButton = popup.querySelector('.popup__close');
-
-let profileTitle = profile.querySelector('.profile__title');
-let profileSubtitle = profile.querySelector('.profile__subtitle');
-
-let inputText = popup.querySelector('.popup__input_text_name');
-let inputAbout = popup.querySelector('.popup__input_text_about');
-
-let popupForm = popup.querySelector('.popup__form');
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  inputText.value = profileTitle.textContent;
-  inputAbout.value = profileSubtitle.textContent;
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = inputText.value;
-  profileSubtitle.textContent = inputAbout.value;
-  closePopup();
-}
-
-popupForm.addEventListener('submit', handleFormSubmit);
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -61,74 +25,122 @@ const initialCards = [
   }
 ];
 
+const profile = document.querySelector('.profile');
+const openPopupButton = profile.querySelector('.profile__edit-button');
+
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
+
 const cardTemplate = document.querySelector('#card').content;
 const placesList = document.querySelector('.places__list');
 
+const popupFullImage = document.querySelector('.popup_full-image');
 
-initialCards.forEach((function cCard(element) {
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+initialCards.forEach(function (element) {
+  createCard(element);
+});
+
+initProfilePopup();
+initAddCard();
+
+function initProfilePopup() {
+  let popupProfile = document.querySelector('.popup_edit-profile');
+  let inputText = popupProfile.querySelector('.popup__input_text_name');
+  let inputAbout = popupProfile.querySelector('.popup__input_text_about');
+  let popupForm = popupProfile.querySelector('.popup__form');
+
+  inputText.value = profileTitle.textContent;
+  inputAbout.value = profileSubtitle.textContent;
+
+  openPopupButton.addEventListener('click', function() {
+    openPopup(popupProfile);
+  });
+
+  popupForm.addEventListener('submit', handleFormSubmit);
+
+  function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = inputText.value;
+    profileSubtitle.textContent = inputAbout.value;
+    closePopup(popupProfile);
+  }
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+
+  let closePopupButton = popup.querySelector('.popup__close');
+
+  closePopupButton.addEventListener('click', function() {
+    closePopup(popup);
+  });
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function initAddCard() {
+  const popupAddCard = document.querySelector('.popup_add-card');
+  const openPopupButtonAddCard = profile.querySelector('.profile__add-button');
+
+  const popupFormAddCard = popupAddCard.querySelector('.popup__form');
+  const inputNameAddCard = popupAddCard.querySelector('.popup__input_text_name-place');
+  const inputLinkAddCard = popupAddCard.querySelector('.popup__input_text_image-link');
+
+  openPopupButtonAddCard.addEventListener('click', function() {
+    openPopup(popupAddCard);
+  });
+
+
+  function handleFormSubmitAddCard(evt) {
+    evt.preventDefault();
+
+    let initialAddCard = {
+      name: inputNameAddCard.value,
+      link: inputLinkAddCard.value
+    };
+
+    createCard(initialAddCard);
+    closePopup(popupAddCard);
+  }
+  popupFormAddCard.addEventListener('submit', handleFormSubmitAddCard);
+}
+
+function createCard(element) {
+  let cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  let deleteButton = cardElement.querySelector('.places__delete-card');
+  let likeButton = cardElement.querySelector('.places__like');
+  let imageCard = cardElement.querySelector('.places__image-container');
 
   cardElement.querySelector('.places__image').src = element.link;
   cardElement.querySelector('.places__image').alt = element.name;
   cardElement.querySelector('.places__text').textContent = element.name;
 
-  placesList.append(cardElement);
+  deleteButton.addEventListener('click', function () {
+    deleteCard(cardElement);
+  });
 
-}));
+  likeButton.addEventListener('click', function () {
+    toggleLike(likeButton);
+  });
 
+  imageCard.addEventListener('click', function() {
+    popupFullImage.querySelector('.popup__image').src = element.link;
+    popupFullImage.querySelector('.popup__heading').textContent = element.name;
 
-/*
-const cardTemplate = document.querySelector('#card').content;
-const placesList = document.querySelector('.places__list');
-const cardElement1 = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardElement2 = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardElement3 = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardElement4 = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardElement5 = cardTemplate.querySelector('.places__item').cloneNode(true);
-const cardElement6 = cardTemplate.querySelector('.places__item').cloneNode(true);
-//контент 1 карточки
+    openPopup(popupFullImage);
+  });
 
-cardElement1.querySelector('.places__image').src = './images/karachaevsk.jpg';
-cardElement1.querySelector('.places__image').alt = 'Старинное здание в горах';
-cardElement1.querySelector('.places__text').textContent = 'Карачаевск';
-cardElement1.querySelector('.places__like').type = 'button';
+  placesList.prepend(cardElement);
+}
 
-//контент 2 карточки
+function deleteCard(elem) {
+  elem.remove();
+}
 
-cardElement2.querySelector('.places__image').src = './images/mount_elbrus.jpg';
-cardElement2.querySelector('.places__image').alt = 'Вид на гору из далека';
-cardElement2.querySelector('.places__text').textContent = 'Гора Эльбрус';
-cardElement2.querySelector('.places__like').type = 'button';
-
-//контент 3 карточки
-
-cardElement3.querySelector('.places__image').src = './images/dombai.jpg';
-cardElement3.querySelector('.places__image').alt = 'Вид на гору за холмом';
-cardElement3.querySelector('.places__text').textContent = 'Домбай';
-cardElement3.querySelector('.places__like').type = 'button';
-
-//контент 4 карточки
-
-cardElement4.querySelector('.places__image').src = './images/mount_elbrus.jpg';
-cardElement4.querySelector('.places__image').alt = 'Вид на гору из далека';
-cardElement4.querySelector('.places__text').textContent = 'Гора Эльбрус';
-cardElement4.querySelector('.places__like').type = 'button';
-
-//контент 5 карточки
-
-cardElement5.querySelector('.places__image').src = './images/dombai.jpg';
-cardElement5.querySelector('.places__image').alt = 'Вид на гору за холмом';
-cardElement5.querySelector('.places__text').textContent = 'Домбай';
-cardElement5.querySelector('.places__like').type = 'button';
-
-//контент 6 карточки
-
-cardElement6.querySelector('.places__image').src = './images/karachaevsk.jpg';
-cardElement6.querySelector('.places__image').alt = 'Старинное здание в горах';
-cardElement6.querySelector('.places__text').textContent = 'Карачаево-Черкесия';
-cardElement6.querySelector('.places__like').type = 'button';
-
-// отображаем на странице
-placesList.append(cardElement1, cardElement2, cardElement3, cardElement4, cardElement5, cardElement6,);//
-*/
+function toggleLike(elem) {
+  elem.classList.toggle('places__like_active');
+}
 
