57f84eb0560f94cc45b63069731f318d5510c036
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-05-13 17:22:41 +0300

    build on webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import {initialCards} from '../utils/initialCards.js';
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
